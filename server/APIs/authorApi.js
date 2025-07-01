8a5e0ab041146dd23caf8e9acd0fff0276a53181
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-01 11:19:47 +0530

    add comment route created

--- server/APIs/authorApi.js
+++ server/APIs/authorApi.js
@@ -23,7 +23,7 @@ authorApp.post("/article",expressAsyncHandler(async(req,res)=>{
 //read all articles
 
 authorApp.get("/articles",expressAsyncHandler(async(req,res)=>{
-    const allArticles=await Article.find({});
+    const allArticles=await Article.find({isArticleActive:true});
     res.status(200).send({message:"all articles",payload:allArticles});
 }))
 
@@ -35,6 +35,16 @@ authorApp.put("/article/:articleId",expressAsyncHandler(async(req,res)=>{
     console.log(updatedArticleObj)
     const updatedArticle=await Article.findOneAndUpdate({articleId},{...updatedArticleObj},{new:true});
     res.status(200).send({message:"article updated",payload:updatedArticle});
+}))
+
+
+authorApp.put("/articleDelete/:articleId",expressAsyncHandler(async(req,res)=>{
+    const articleId=req.params.articleId;
+    console.log(articleId);
+    const updatedArticleObj=req.body; 
+    console.log(updatedArticleObj)
+    const updatedArticle=await Article.findOneAndUpdate({articleId},{...updatedArticleObj},{new:true});
+    res.status(200).send({message:"article deleted",payload:updatedArticle});
 }))
 
 module.exports=authorApp;
--- server/APIs/userApi.js
+++ server/APIs/userApi.js
@@ -3,10 +3,17 @@ const userApp=exp.Router()
 const User=require('../models/userModel')
 const expressAsyncHandler=require('express-async-handler')
 const createUser=require('../middlewares/createUser')
+const Article = require('../models/articleModel')
 //create a new user
 userApp.post("/user",expressAsyncHandler(createUser))
 
+//add comment
 
+userApp.put('/comment/:articleID',expressAsyncHandler(async(req,res)=>{
+    const commentObj=req.body;
+    const modifiedArticle=await Article.findOneAndUpdate({articleId:req.params.articleID},{$push:{comments:commentObj}},{new:true});
+    res.status(200).send({message:"comment added",payload:modifiedArticle});
+}))
 
 
 
