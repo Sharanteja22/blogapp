7ba3c797e81d49e425e7b26bedd8fb6d751d73f9
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-02 17:15:19 +0530

    userprofile created

--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import {Link,Outlet} from 'react-router-dom'
 
 function UserProfile() {
   return (
-    <div>UserProfile</div>
+    <div className="w-100 d-flex flex-column" >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="">
+            Author
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" to="articles">
+                  Articles
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="flex-fill d-flex  "
+          style={{
+            minHeight: 0, // This allows flex item to shrink below content size
+            flex: 1,
+          }}>
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
-export default UserProfile
+export default UserProfile  
