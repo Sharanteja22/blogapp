9274eec5094c67b5c7ab99e1308bf92a76d5a1cd
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-04 15:12:24 +0530

    Fix Clerk requireAuth with full signInUrl

--- server/APIs/authorApi.js
+++ server/APIs/authorApi.js
@@ -1,93 +1,93 @@
-const exp = require('express');
-const authorApp = exp.Router();
-const expressAsyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
-const createUser = require('../middlewares/createUser');
-const Article = require('../models/articleModel');
-const { requireAuth } = require('@clerk/express');
-require('dotenv').config();
+const exp = require("express")
+const authorApp = exp.Router()
+const expressAsyncHandler = require("express-async-handler")
+const User = require("../models/userModel")
+const createUser = require("../middlewares/createUser")
+const Article = require("../models/articleModel")
+const { requireAuth } = require("@clerk/express")
+require("dotenv").config()
 
 // Create a new author
-authorApp.post('/user', expressAsyncHandler(createUser));
+authorApp.post("/user", expressAsyncHandler(createUser))
 
 // Create a new article
 authorApp.post(
-  '/article',
+  "/article",
   expressAsyncHandler(async (req, res) => {
-    const newArticle = new Article(req.body);
-    const savedArticle = await newArticle.save();
-    res.status(201).send({ message: 'article created', payload: savedArticle });
-  })
-);
+    const newArticle = new Article(req.body)
+    const savedArticle = await newArticle.save()
+    res.status(201).send({ message: "article created", payload: savedArticle })
+  }),
+)
 
 // Read all articles (active)
 authorApp.get(
-  '/articles',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/articles",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
-    const articles = await Article.find({ isArticleActive: true });
-    res.status(200).send({ message: 'articles', payload: articles });
-  })
-);
+    const articles = await Article.find({ isArticleActive: true })
+    res.status(200).send({ message: "articles", payload: articles })
+  }),
+)
 
-// Read only this author’s articles
+// Read only this author's articles
 authorApp.get(
-  '/articles/:authorName',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/articles/:authorName",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
     const articles = await Article.find({
-      'authorData.nameOfAuthor': req.params.authorName,
+      "authorData.nameOfAuthor": req.params.authorName,
       isArticleActive: true,
-    });
-    res.status(200).send({ message: 'articles by author', payload: articles });
-  })
-);
+    })
+    res.status(200).send({ message: "articles by author", payload: articles })
+  }),
+)
 
 // Read deleted articles
 authorApp.get(
-  '/deletedArticles/:authorName',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/deletedArticles/:authorName",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
     const articles = await Article.find({
-      'authorData.nameOfAuthor': req.params.authorName,
+      "authorData.nameOfAuthor": req.params.authorName,
       isArticleActive: false,
-    });
-    res.status(200).send({ message: 'deleted articles by author', payload: articles });
-  })
-);
+    })
+    res.status(200).send({ message: "deleted articles by author", payload: articles })
+  }),
+)
 
 // Edit an article
 authorApp.put(
-  '/article/:articleId',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/article/:articleId",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
     const updatedArticle = await Article.findOneAndUpdate(
       { articleId: req.params.articleId },
       { ...req.body },
-      { new: true }
-    );
-    res.status(200).send({ message: 'article updated', payload: updatedArticle });
-  })
-);
+      { new: true },
+    )
+    res.status(200).send({ message: "article updated", payload: updatedArticle })
+  }),
+)
 
 // Soft delete or restore article
 authorApp.put(
-  '/articleDelete/:articleId',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/articleDelete/:articleId",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
     const updatedArticle = await Article.findOneAndUpdate(
       { articleId: req.params.articleId },
       { ...req.body },
-      { new: true }
-    );
-    const msg = updatedArticle.isArticleActive ? 'article restored' : 'article deleted';
-    res.status(200).send({ message: msg, payload: updatedArticle });
-  })
-);
+      { new: true },
+    )
+    const msg = updatedArticle.isArticleActive ? "article restored" : "article deleted"
+    res.status(200).send({ message: msg, payload: updatedArticle })
+  }),
+)
 
 // Unauthorized page
-authorApp.get('/unauthorized', (req, res) => {
-  res.status(401).send({ message: 'Unauthorized. Please log in.' });
-});
+authorApp.get("/unauthorized", (req, res) => {
+  res.status(401).send({ message: "Unauthorized. Please log in." })
+})
 
-module.exports = authorApp;
+module.exports = authorApp
--- server/APIs/userApi.js
+++ server/APIs/userApi.js
@@ -1,33 +1,33 @@
-const exp = require('express');
-const userApp = exp.Router();
-const User = require('../models/userModel');
-const expressAsyncHandler = require('express-async-handler');
-const createUser = require('../middlewares/createUser');
-const Article = require('../models/articleModel');
-const { requireAuth } = require('@clerk/express');
-require('dotenv').config();
+const exp = require("express")
+const userApp = exp.Router()
+const User = require("../models/userModel")
+const expressAsyncHandler = require("express-async-handler")
+const createUser = require("../middlewares/createUser")
+const Article = require("../models/articleModel")
+const { requireAuth } = require("@clerk/express")
+require("dotenv").config()
 
 // Create a new user
-userApp.post('/user', expressAsyncHandler(createUser));
+userApp.post("/user", expressAsyncHandler(createUser))
 
 // Add comment
 userApp.put(
-  '/comment/:articleID',
-  requireAuth({ signInUrl: 'https://blogapp-to47.onrender.com/unauthorized' }),
+  "/comment/:articleID",
+  requireAuth({ signInUrl: "/unauthorized" }), // ✅ Fixed: relative path
   expressAsyncHandler(async (req, res) => {
-    const commentObj = req.body;
+    const commentObj = req.body
     const modifiedArticle = await Article.findOneAndUpdate(
       { articleId: req.params.articleID },
       { $push: { comments: commentObj } },
-      { new: true }
-    );
-    res.status(200).send({ message: 'comment added', payload: modifiedArticle });
-  })
-);
+      { new: true },
+    )
+    res.status(200).send({ message: "comment added", payload: modifiedArticle })
+  }),
+)
 
 // Unauthorized page
-userApp.get('/unauthorized', (req, res) => {
-  res.status(401).send({ message: 'Unauthorized. Please log in.' });
-});
+userApp.get("/unauthorized", (req, res) => {
+  res.status(401).send({ message: "Unauthorized. Please log in." })
+})
 
-module.exports = userApp;
+module.exports = userApp
